1095b177fe2d3ee1e392309fd5f4685321f0166d
miya24kou01 <miya24_kou01@outlook.jp>
2024-05-26 20:47:07 +0900

    kadai_011の提出

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題011</title>
+</head>
+<body>
+<p>
+ <?php
+ $personal_data=['name'=>'玉ねぎ','price'=>200,'place'=>'北海道'];
+ foreach($personal_data as $key=>$value){
+    echo"{$key}:{$value}<br>";
+ }
+ ?>
+</p>
+
+</body>
+</html>
