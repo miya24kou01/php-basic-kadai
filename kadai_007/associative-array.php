4da83dff23cc4cc833ec5f71fae32e5383722916
miya24kou01 <miya24_kou01@outlook.jp>
2024-05-26 20:41:19 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+  <P>
+    <?php
+    $personal_data=['name'=>'onion','price'=>200,'weight'=>'160'];
+    print_r($personal_data)
+    ?>
+  </P>  
+</body>
+</html>
