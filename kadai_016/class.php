2cb6ead547fe1716728265bbb09458e454396755
miya24kou01 <miya24_kou01@outlook.jp>
2024-05-27 23:03:56 +0900

    kadai_016再提出

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -1,38 +1,47 @@
-<?php
-class Food {
-    private $name;
-    private $price;
-    public function show_price(int $price) {
-        $this->price = $price;
-    }
-    public function __construct(string $name, int $price) {
-        $this->name = $name;
-        $this->price =$price;
-    }   
-}
-$food = new Food('potato', 250);
-print_r($food);
-echo '<br>';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題016</title>
+</head>
+<body>
+    <p>
+        <?php
+        class Food {
+            private $name;
+            private $price;
+            public function show_price() {
+                echo $this->price . '<br>';
+            }
+            public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+            }
+        }
+        $food = new Food('potato' ,250);
+        print_r($food);
+        echo '<br>';
 
-class Animal {
-    private $name;
-    private $height;
-    private $weight;
-    public function show_height(int $height) {
-        $this->height = $height;
-    }
-    public function __construct(string $name, int $height, int $weight) {
-            $this->name = $name; 
-            $this->height = $height;
-            $this->weight = $weight;
-    }     
-}
-
-$animal= new Animal('dog', 60, 5000);
-print_r($animal);
-
-$food->show_price(250);
-
-$animal->show_height(60);
-
-?>
+        class Animal {
+            public $name;
+            public $height;
+            public $weight;
+            public function show_height() {
+                echo $this->height . '<br>';
+            }
+            public function __construct(string $name, int $height, string $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+        }
+        $animal = new Animal('dog' ,60, 5000);
+        print_r($animal);
+        echo '<br>';
+        $food->show_price();
+        $animal->show_height();
+        
+        ?>
+    </p>
+</body>
+</html>
